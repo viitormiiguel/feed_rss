35f2fc5d0cbefc88eebd2dcbad5a82c170787f7d
root <root@DESKTOP-NGOJP5G.localdomain>
2018-09-10 20:49:20 +0000

    ajustes parser

--- src/main/java/read_rss/data/InvestingData.java
+++ src/main/java/read_rss/data/InvestingData.java
@@ -18,62 +18,13 @@ import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 
-public class InvestingData {
-	
-	 public static String [][] htmlEscape =
-	{{  "&lt;"     , "<" } ,  {  "&gt;"     , ">" }, {  "&amp;"    , "&" } ,  {  "&quot;"   , "\"" } ,
-	  {  "&agrave;" , "à" } ,  {  "&Agrave;" , "À" }, {  "&acirc;"  , "â" } ,  {  "&auml;"   , "ä" } ,
-	  {  "&Auml;"   , "Ä" } ,  {  "&Acirc;"  , "Â" } , {  "&aring;"  , "å" } ,  {  "&Aring;"  , "Å" } ,
-	  {  "&aelig;"  , "æ" } ,  {  "&AElig;"  , "Æ" } , {  "&ccedil;" , "ç" } ,  {  "&Ccedil;" , "Ç" } ,
-	  {  "&eacute;" , "é" } ,  {  "&Eacute;" , "É" } , {  "&egrave;" , "è" } ,  {  "&Egrave;" , "È" } ,
-	  {  "&ecirc;"  , "ê" } ,  {  "&Ecirc;"  , "Ê" } , {  "&euml;"   , "ë" } ,  {  "&Euml;"   , "Ë" } ,
-	  {  "&iuml;"   , "ï" } ,  {  "&Iuml;"   , "Ï" } , {  "&ocirc;"  , "ô" } ,  {  "&Ocirc;"  , "Ô" } ,
-	  {  "&ouml;"   , "ö" } ,  {  "&Ouml;"   , "Ö" } , {  "&oslash;" , "ø" } ,  {  "&Oslash;" , "Ø" } ,
-	  {  "&szlig;"  , "ß" } ,  {  "&ugrave;" , "ù" } , {  "&Ugrave;" , "Ù" } ,  {  "&ucirc;"  , "û" } ,
-	  {  "&Ucirc;"  , "Û" } ,  {  "&uuml;"   , "ü" } , {  "&Uuml;"   , "Ü" } ,  {  "&nbsp;"   , " " } ,
-	  {  "&copy;"   , "\u00a9" } , { "&atilde;", "ã" }, {  "&reg;"    , "\u00ae" } , { "&Atilde;", "Â" },
-	  {  "&euro;"   , "\u20a0" } , {"&Agrave;" ,"À"}, {  "&Aacute;" , "Á" }, {  "&Acirc;" , "Â" },
-	  {  "&Atilde;" , "Ã" }, {  "&Auml;"   ,"Ä" },    {  "&Aring;"  ,"Å" }, {  "&AElig;"  ,"Æ"}, 
-	  {  "&Ccedil;" , "Ç" }, {  "&Egrave;" ,"È" },    {  "&Eacute;" ,"É" }, {  "&Ecirc;"  ,"Ê"}, 
-	  {  "&Euml;"   , "Ë" }, {  "&Igrave;" ,"Ì" },    {  "&Iacute;" ,"Í" }, {  "&Icirc;"  ,"Î"}, 
-	  {  "&Iuml;"   , "Ï" }, {  "&ETH;"    ,"Ð" },    {  "&Ntilde;" ,"Ñ" }, {  "&Ograve;" ,"Ò"},
-	  {  "&Oacute;" , "Ó" }, {  "&Ocirc;"  ,"Ô" }, 	  {  "&Otilde;" ,"Õ" }, {  "&Ouml;"   ,"Ö"}, 
-	  {  "&Oslash;" , "Ø" }, {  "&Ugrave;" ,"Ù" }, 	  {  "&Uacute;" ,"Ú" }, {  "&Ucirc;"  ,"Û"}, 
-	  {  "&Uuml;"   , "Ü" }, {  "&Yacute;" ,"Ý" }, 	  {  "&THORN;"  ,"Þ" }, {  "&szlig;"  ,"ß"}, 
-	  {  "&agrave;" , "à" }, {  "&aacute;" ,"á" }, 	  {  "&acirc;"  ,"â" }, {  "&atilde;" ,"ã"},
-	  {  "&auml;"   , "ä" }, {  "&aring;"  ,"å" }, 	  {  "&aelig;"  ,"æ" }, {  "&ccedil;" ,"ç"},
-	  {  "&egrave;" , "è" }, {  "&eacute;" ,"é" }, 	  {  "&ecirc;"  ,"ê" }, {  "&euml;"   ,"ë"},
-	  {  "&igrave;" , "ì" }, {  "&iacute;" ,"í" }, 	  {  "&icirc;"  ,"î" }, {  "&iuml;"   ,"ï"}, 
-	  {  "&eth;"    , "ð" }, {  "&ntilde;" ,"ñ" }, 	  {  "&ograve;" ,"ò" }, {  "&oacute;" ,"ó"}, 
-	  {  "&ocirc;"  , "ô" }, {  "&otilde;" ,"õ" }, 	  {  "&ouml;"   ,"ö" }, {  "&oslash;" ,"ø"}, 
-	  {  "&ugrave;" , "ù" }, {  "&uacute;" ,"ú" },    {  "&ucirc;"  ,"û" }, {  "&uuml;" ,  "ü"}, 
-	  {  "&yacute;" , "ý" }, {  "&thorn;"  ,"þ" },    {  "&yuml;"   ,"ÿ" }
-   };
+import read_rss.data.Subs;
 
-	 public static final String unescapeHTML(String s, int start){
-	 	 int i, j, k;
-	     i 		= s.indexOf("&", start);
-	     start 	= i + 1;	     
-	     if (i > -1) {
-	    	 j = s.indexOf(";" ,i);
-	    	 if (j > i) {
-	    		 String temp = s.substring(i , j + 1);
-	    		 k = 0;
-	    		 while (k < htmlEscape.length) {
-	    			 if (htmlEscape[k][0].equals(temp)) break;
-	    			 else k++;
-	    		 }	
-	    		 if (k < htmlEscape.length) {
-	    			 s = s.substring(0 , i)
-	                 + htmlEscape[k][1] + s.substring(j + 1);
-	    			 return unescapeHTML(s, i);
-	    		 }
-	         }
-	     }
-	     return s;
-	  }
+public class InvestingData{
 	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	final Subs frmt = new Subs();
+	
+	@SuppressWarnings({ "rawtypes", "unchecked", "static-access" })
 	public String Investing (String tipo, String link) throws IOException, IllegalArgumentException, FeedException {
 		
 		String status = null;
@@ -96,7 +47,7 @@ public class InvestingData {
 			for (Iterator i = feed.getEntries().iterator(); i.hasNext();) {
 				SyndEntry entry = (SyndEntry) i.next();				
 				JSONObject tec1 	= new JSONObject();
-				tec1.put("title", unescapeHTML(entry.getTitle(), 0));
+				tec1.put("title", frmt.unescapeHTML(entry.getTitle(), 0));
 				tec1.put("author", entry.getAuthor());				
 				list_im.add(tec1);								
 				try (FileWriter file = new FileWriter(fileName + tipo + ".json")) {	    		
--- src/main/java/read_rss/fedd/App.java
+++ src/main/java/read_rss/fedd/App.java
@@ -1,24 +1,18 @@
 package read_rss.fedd;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import read_rss.data.InfoMoneyData;
 import read_rss.data.InvestingData;
 import read_rss.data.TradingViewData;
-import read_rss.data.TesteRead;
 
 public class App {
 	
     public static void main( String[] args ) throws Exception {
     	
-//    	@SuppressWarnings("unused")
 		final TradingViewData sampleT 	= new TradingViewData();
-    	//@SuppressWarnings("unused")
-//		final InfoMoneyData sampleM 	= new InfoMoneyData();
-//    	@SuppressWarnings("unused")
-//		final InvestingData sampleC		= new InvestingData();
+		final InfoMoneyData sampleM 	= new InfoMoneyData();
+		final InvestingData sampleC		= new InvestingData();
     	
+		// ACÇÕES INDIVIDUAIS
     	sampleT.TradingView("jbss3");
     	sampleT.TradingView("petr4");
     	sampleT.TradingView("aapl");
@@ -27,17 +21,19 @@ public class App {
     	sampleT.TradingView("btcusd");
     	sampleT.TradingView("natu3");
     	sampleT.TradingView("ltcbrl");
-//    	    	
-//    	sampleM.InfoMoney("acoes", "https://www.infomoney.com.br/onde-investir/acoes/rss");
-//    	sampleM.InfoMoney("analise-tecnica", "https://www.infomoney.com.br/mercados/analise-tecnica/rss");
-//    	sampleM.InfoMoney("bitcoin", "https://www.infomoney.com.br/mercados/bitcoin/rss");
-//    	sampleM.InfoMoney("acoes-indices", "https://www.infomoney.com.br/mercados/acoes-e-indices/rss");
-//    	
-//    	sampleC.Investing("analise-tecnica-mercado", "https://br.investing.com/rss/market_overview_Technical.rss");
-//    	sampleC.Investing("analise-funda-mercado", "https://br.investing.com/rss/market_overview_Fundamental.rss");
-//    	sampleC.Investing("analise-tecnica-acoes", "https://br.investing.com/rss/stock_Technical.rss");
-//    	sampleC.Investing("analise-funda-acoes", "https://br.investing.com/rss/stock_Fundamental.rss");
-//    	sampleC.Investing("opiniao-mercado", "https://br.investing.com/rss/stock_Opinion.rss"); 
+    	sampleT.TradingView("ibov");
+    	sampleT.TradingView("itsa4");
+    	    	    	
+    	sampleM.InfoMoney("acoes", "https://www.infomoney.com.br/onde-investir/acoes/rss");
+    	sampleM.InfoMoney("analise-tecnica", "https://www.infomoney.com.br/mercados/analise-tecnica/rss");
+    	sampleM.InfoMoney("bitcoin", "https://www.infomoney.com.br/mercados/bitcoin/rss");
+    	sampleM.InfoMoney("acoes-indices", "https://www.infomoney.com.br/mercados/acoes-e-indices/rss");
+    	
+    	sampleC.Investing("analise-tecnica-mercado", "https://br.investing.com/rss/market_overview_Technical.rss");
+    	sampleC.Investing("analise-funda-mercado", "https://br.investing.com/rss/market_overview_Fundamental.rss");
+    	sampleC.Investing("analise-tecnica-acoes", "https://br.investing.com/rss/stock_Technical.rss");
+    	sampleC.Investing("analise-funda-acoes", "https://br.investing.com/rss/stock_Fundamental.rss");
+    	sampleC.Investing("opiniao-mercado", "https://br.investing.com/rss/stock_Opinion.rss"); 
 
     }
     
