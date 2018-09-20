a08ccf4a989a76cf90afca451b11dfe323c2ea5a
root <root@DESKTOP-NGOJP5G.localdomain>
2018-09-20 17:45:59 +0000

    descomentando codigo

--- src/main/java/read_rss/fedd/App.java
+++ src/main/java/read_rss/fedd/App.java
@@ -14,59 +14,59 @@ public class App {
 		final InvestingData sampleC		= new InvestingData();
 		
 		// ACÇÕES INDIVIDUAIS
-//    	sampleT.TradingView("jbss3");
-//    	sampleT.TradingView("petr4");
-//    	sampleT.TradingView("aapl");
-//    	sampleT.TradingView("itub4");
-//    	sampleT.TradingView("vale3");
-//    	sampleT.TradingView("btcusd");
-//    	sampleT.TradingView("natu3");
-//    	sampleT.TradingView("ltcbrl");
-//    	sampleT.TradingView("ibov");
-//    	sampleT.TradingView("itsa4");
-//    	sampleT.TradingView("goll4");
-//    	sampleT.TradingView("bbas3");
-//    	sampleT.TradingView("abev3");
-//		
-//    	sampleT.csvWriter("jbss3");
-//    	sampleT.csvWriter("petr4");
-//    	sampleT.csvWriter("aapl");
-//    	sampleT.csvWriter("itub4");
-//    	sampleT.csvWriter("vale3");
-//    	sampleT.csvWriter("btcusd");
-//    	sampleT.csvWriter("natu3");
-//    	sampleT.csvWriter("ltcbrl");
-//    	sampleT.csvWriter("ibov");
-//    	sampleT.csvWriter("itsa4");
-//    	sampleT.csvWriter("goll4");
-//    	sampleT.csvWriter("bbas3");
-//    	sampleT.csvWriter("abev3");
+    	sampleT.TradingView("jbss3");
+    	sampleT.TradingView("petr4");
+    	sampleT.TradingView("aapl");
+    	sampleT.TradingView("itub4");
+    	sampleT.TradingView("vale3");
+    	sampleT.TradingView("btcusd");
+    	sampleT.TradingView("natu3");
+    	sampleT.TradingView("ltcbrl");
+    	sampleT.TradingView("ibov");
+    	sampleT.TradingView("itsa4");
+    	sampleT.TradingView("goll4");
+    	sampleT.TradingView("bbas3");
+    	sampleT.TradingView("abev3");
+		
+    	sampleT.csvWriter("jbss3");
+    	sampleT.csvWriter("petr4");
+    	sampleT.csvWriter("aapl");
+    	sampleT.csvWriter("itub4");
+    	sampleT.csvWriter("vale3");
+    	sampleT.csvWriter("btcusd");
+    	sampleT.csvWriter("natu3");
+    	sampleT.csvWriter("ltcbrl");
+    	sampleT.csvWriter("ibov");
+    	sampleT.csvWriter("itsa4");
+    	sampleT.csvWriter("goll4");
+    	sampleT.csvWriter("bbas3");
+    	sampleT.csvWriter("abev3");
     	
-//    	sampleT.mergeFile();
+    	sampleT.mergeFile();
 
-//    	sampleM.InfoMoney("acoes", "https://www.infomoney.com.br/onde-investir/acoes/rss");
-//    	sampleM.InfoMoney("analise-tecnica", "https://www.infomoney.com.br/mercados/analise-tecnica/rss");
-//    	sampleM.InfoMoney("bitcoin", "https://www.infomoney.com.br/mercados/bitcoin/rss");
-//    	sampleM.InfoMoney("acoes-indices", "https://www.infomoney.com.br/mercados/acoes-e-indices/rss");
+    	sampleM.InfoMoney("acoes", "https://www.infomoney.com.br/onde-investir/acoes/rss");
+    	sampleM.InfoMoney("analise-tecnica", "https://www.infomoney.com.br/mercados/analise-tecnica/rss");
+    	sampleM.InfoMoney("bitcoin", "https://www.infomoney.com.br/mercados/bitcoin/rss");
+    	sampleM.InfoMoney("acoes-indices", "https://www.infomoney.com.br/mercados/acoes-e-indices/rss");
     	
-//    	sampleM.csvWriter("acoes");
-//    	sampleM.csvWriter("analise-tecnica");
-//    	sampleM.csvWriter("bitcoin");
-//    	sampleM.csvWriter("acoes-indices");
+    	sampleM.csvWriter("acoes");
+    	sampleM.csvWriter("analise-tecnica");
+    	sampleM.csvWriter("bitcoin");
+    	sampleM.csvWriter("acoes-indices");
     	
-//    	sampleM.mergeFile();
-//    	
-//    	sampleC.Investing("analise-tecnica-mercado", "https://br.investing.com/rss/market_overview_Technical.rss");
-//    	sampleC.Investing("analise-funda-mercado", "https://br.investing.com/rss/market_overview_Fundamental.rss");
-//    	sampleC.Investing("analise-tecnica-acoes", "https://br.investing.com/rss/stock_Technical.rss");
-//    	sampleC.Investing("analise-funda-acoes", "https://br.investing.com/rss/stock_Fundamental.rss");
-//    	sampleC.Investing("opiniao-mercado", "https://br.investing.com/rss/stock_Opinion.rss"); 
-//		
-//		sampleC.csvWriter("analise-tecnica-mercado");
-//		sampleC.csvWriter("analise-funda-mercado");
-//		sampleC.csvWriter("analise-tecnica-acoes");
-//		sampleC.csvWriter("analise-funda-acoes");
-//		sampleC.csvWriter("opiniao-mercado");
+    	sampleM.mergeFile();
+    	
+    	sampleC.Investing("analise-tecnica-mercado", "https://br.investing.com/rss/market_overview_Technical.rss");
+    	sampleC.Investing("analise-funda-mercado", "https://br.investing.com/rss/market_overview_Fundamental.rss");
+    	sampleC.Investing("analise-tecnica-acoes", "https://br.investing.com/rss/stock_Technical.rss");
+    	sampleC.Investing("analise-funda-acoes", "https://br.investing.com/rss/stock_Fundamental.rss");
+    	sampleC.Investing("opiniao-mercado", "https://br.investing.com/rss/stock_Opinion.rss"); 
+		
+		sampleC.csvWriter("analise-tecnica-mercado");
+		sampleC.csvWriter("analise-funda-mercado");
+		sampleC.csvWriter("analise-tecnica-acoes");
+		sampleC.csvWriter("analise-funda-acoes");
+		sampleC.csvWriter("opiniao-mercado");
 		
 		sampleC.mergeFile();
 
